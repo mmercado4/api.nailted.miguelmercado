7923c0be899e1b311ee4d30eafd1abc42541d425
MIGUEL MERCADO SALGADO <mmercado@accom.local>
2022-08-29 23:16:22 +0200

    feat(api): get employees by pages

--- api.ts
+++ api.ts
@@ -2,32 +2,50 @@ import { apiInit } from "./src/config/apiConfig";
 import { Request, Response } from "express";
 import fs from "fs";
 import { Employee } from "./src/IEmployee";
+import { ParsedQs } from "qs";
 
 const api = apiInit();
 const PORT = 3001;
 
-api.get("/employees", (request: Request, response: Response) => {
-  try {
-    let employees: Array<Employee> = fs
-      .readFileSync("employees.txt", "utf-8")
-      .split("\n")
-      .map((data: String) => {
-        let employee: Array<string> = data.split(",");
-        return {
-          id: parseInt(employee[0]),
-          name: employee[1],
-          surname: employee[2],
-          address: employee[3],
-          phone: employee[4],
-          email: employee[5],
-          birth: employee[6],
-        };
-      });
-    return response.status(200).send(employees);
-  } catch (error) {
-    console.error("Something went wrong: ", error);
+interface Pagination {
+  offset: number | string;
+  limit: number | string;
+}
+
+api.get(
+  "/employees",
+  (request: Request<any, any, ParsedQs>, response: Response) => {
+    try {
+      let employees: Array<Employee> = fs
+        .readFileSync("employees.txt", "utf-8")
+        .split("\n")
+        .map((data: String) => {
+          let employee: Array<string> = data.split(",");
+          return {
+            id: parseInt(employee[0]),
+            name: employee[1],
+            surname: employee[2],
+            address: employee[3],
+            phone: employee[4],
+            email: employee[5],
+            birth: employee[6],
+          };
+        });
+
+      let query: Pagination | string | ParsedQs | string[] | ParsedQs[] =
+        Object.keys(request.query).length > 0
+          ? request.query
+          : { offset: 0, limit: employees.length };
+      let start: number = parseInt(query.offset as string);
+      let end: number =
+        parseInt(query.offset as string) + parseInt(query.limit as string);
+
+      return response.status(200).send(employees.slice(start, end));
+    } catch (error) {
+      console.error("Something went wrong: ", error);
+    }
   }
-});
+);
 
 api.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}/`)
