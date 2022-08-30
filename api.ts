7b30386798988b86e2863ab286d04dbda8a48e48
MIGUEL MERCADO SALGADO <mmercado@accom.local>
2022-08-30 18:24:58 +0200

    feat(config): prepare for deploy

--- api.ts
+++ api.ts
@@ -5,7 +5,7 @@ import { Employee } from "./src/IEmployee";
 import { ParsedQs } from "qs";
 
 const api = apiInit();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 interface Pagination {
   offset: number | string;
