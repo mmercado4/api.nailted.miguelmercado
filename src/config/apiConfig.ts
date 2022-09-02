ccef43e5ae970c3cd678316363966af64580e244
MIGUEL MERCADO SALGADO <mmercado@accom.local>
2022-09-02 17:31:47 +0200

    feat(cors): set cors policy

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,11 +1,20 @@
 const express = require("express");
 const routes = require("../routes/employee");
+const cors = require("cors");
 
 const apiInit = () => {
   const api = express();
 
   api.use(express.json());
   api.use(express.urlencoded({ extended: true }));
+
+  // let corsOptions = {
+  //   origin: ["*"],
+  //   methods: ["GET", "PATCH", "PUT", "POST", "DELETE", "OPTIONS"],
+  //   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+  // };
+
+  api.use(cors());
 
   api.use("/", routes);
 
